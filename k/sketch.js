4e21511a7592e544546f369e3af7b1cff098a660
joana-franco <jfranco.emailalternativo@gmail.com>
2026-06-03 17:40:13 +0100

    Create sketch.js

---
+++ k/sketch.js
@@ -0,0 +1,127 @@
+let size = 100;
+let margin = 200;
+let bg = 0;
+let ac = 255;
+let smoothOffset = 0;
+
+let shapes = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(bg);
+
+  noFill();
+  stroke(ac);
+  strokeWeight(size / 10);
+  angleMode(DEGREES);
+
+  shapes.push({
+    shape: new Shape(1 * size),
+    p1: { x: 0, y: -0.5 * size },
+    p2: { x: 0, y: -0.5 * size },
+    p3: { x: 0.2 * size, y: 0 },
+    r1: 90,
+    r2: 0,
+    r3: 0,
+  });
+
+  shapes.push({
+    shape: new Shape(0.8 * size),
+    p1: { x: 0, y: 0 },
+    p2: { x: 0.5 * size, y: 0.2 * size },
+    p3: { x: 1.9 * size, y: 0 },
+    r1: -38,
+    r2: 45,
+    r3: 135,
+  });
+
+  shapes.push({
+    shape: new Shape(0.8 * size),
+    p1: { x: 0, y: 0 },
+    p2: { x: 0.5 * size, y: 0.2 * size },
+    p3: { x: 1.9 * size, y: 0 },
+    r1: 38,
+    r2: 135,
+    r3: 225,
+  });
+
+  shapes.push({
+    shape: new Shape(0.7 * size),
+    p1: { x: size, y: -0.5 * size },
+    p2: { x: 0.5 * size, y: -0.5 * size },
+    p3: { x: 1.2 * size, y: 0 },
+    r1: 90,
+    r2: 90,
+    r3: 0,
+  });
+
+  shapes.push({
+    shape: new Shape(0 * size),
+    p1: { x: size, y: 0.5 * size },
+    p2: { x: 0.5 * size, y: -0.8 * size },
+    p3: { x: 0, y: 0 },
+    r1: 0,
+    r2: 0,
+    r3: 0,
+  });
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(bg);
+
+  let targetOffset = map(mouseX, margin, width - margin, 0, 1, true);
+
+  smoothOffset = lerp(smoothOffset, targetOffset, 0.3);
+
+  let t = map(
+    mouseX,
+    margin,
+    width - margin,
+    margin,
+    width - margin - size,
+    true
+  );
+
+  translate(t, height * 0.5);
+
+  shapes.forEach((s) => {
+    s.shape.display(s.p1, s.p2, s.p3, s.r1, s.r2, s.r3, smoothOffset);
+  });
+}
+
+class Shape {
+  constructor(w) {
+    this.w = w;
+  }
+
+  display(p1, p2, p3, r1, r2, r3, offset) {
+    push();
+
+    let x, y, r;
+    if (offset < 0.4) {
+      let t = map(offset, 0, 0.4, 0, 1, true);
+      x = lerp(p1.x, p2.x, t);
+      y = lerp(p1.y, p2.y, t);
+      r = lerp(r1, r2, t);
+    } else if (offset < 0.6) {
+      x = p2.x;
+      y = p2.y;
+      r = r2;
+    } else {
+      let t = map(offset, 0.6, 1, 0, 1, true);
+      x = lerp(p2.x, p3.x, t);
+      y = lerp(p2.y, p3.y, t);
+      r = lerp(r2, r3, t);
+    }
+
+    translate(x, y);
+    rotate(r);
+    line(0, 0, this.w, 0);
+
+    pop();
+  }
+}
