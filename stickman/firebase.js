3a05d949d1465c9080ab704b315478c0c3cc4045
joana-franco <jfranco.emailalternativo@gmail.com>
2026-06-25 00:05:43 +0100

    Update firebase.js

--- stickman/firebase.js
+++ stickman/firebase.js
@@ -9,11 +9,13 @@ import {
   orderBy
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// 🔥 YOUR CONFIG HERE
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyDHj0gbdNNCuNeqYLRG32-uQF--yrEoSH8",
+  authDomain: "kaput-emlinha.firebaseapp.com",
+  projectId: "kaput-emlinha",
+  storageBucket: "kaput-emlinha.firebasestorage.app",
+  messagingSenderId: "461983977669",
+  appId: "1:461983977669:web:9febf826695918f5eda0ac"
 };
 
 // Initialize Firebase
